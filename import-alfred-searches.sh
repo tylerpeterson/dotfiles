f0c95fa7800ec144f1fedf8cb7b11cb46dff7213
Tyler Peterson <stronglytyped@gmail.com>
2018-10-31 12:26:27 -0600

    Document custom searches in Alfred.
    Run the shell script to trigger automatic importing of the custom searches.

---
+++ import-alfred-searches.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+open "alfred://customsearch/Search FHD Confluence/confluence/utf8/noplus/https://almtools.ldschurch.org/fhconfluence/dosearchsite.action?queryString={query}"
+open "alfred://customsearch/Search FHD VersionOne/v1/utf8/noplus/https://www5.v1host.com/FH-V1/Search.mvc/Advanced?q={query}"
+open "alfred://customsearch/Lookup Jira by ID/jira/utf8/noplus/https://almtools.ldschurch.org/fhjira/browse/{query}"
