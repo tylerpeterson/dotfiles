a8e80660d342260540852a01324975b531b8c828
Tyler Peterson <stronglytyped@gmail.com>
2020-06-16 15:14:43 -0600

    Improve nvm setup script.
    Make sure to set it executable or it won't be sourced.

--- bashrc.d/brewed-nvm.sh
+++ bashrc.d/brewed-nvm.sh
@@ -1,4 +1,5 @@
 # See $ brew info nvm
 
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
