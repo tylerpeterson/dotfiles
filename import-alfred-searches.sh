9fb9fe11896e5213d504806821eaaed987f1cc06
Tyler Peterson <stronglytyped@gmail.com>
2021-07-30 14:52:55 -0600

    Update alfred search urls
    Domain names changed.
    Couldn't get the other's to open. Perhaps the more escaped URLs that Afred makes now will work better.

--- import-alfred-searches.sh
+++ import-alfred-searches.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-open "alfred://customsearch/Search FHD Confluence/confluence/utf8/noplus/https://almtools.ldschurch.org/fhconfluence/dosearchsite.action?queryString={query}"
-open "alfred://customsearch/Search FHD VersionOne/v1/utf8/noplus/https://www5.v1host.com/FH-V1/Search.mvc/Advanced?q={query}"
-open "alfred://customsearch/Lookup Jira by ID/jira/utf8/noplus/https://almtools.ldschurch.org/fhjira/browse/{query}"
+open "alfred://customsearch/Search%20FHD%20Confluence/confluence/utf8/nospace/https%3A%2F%2Ffhconfluence.churchofjesuschrist.org%2Fdosearchsite.action%3FqueryString%3D%7Bquery%7D"
+open "alfred://customsearch/Search%20FHD%20VersionOne/v1/utf8/nospace/https%3A%2F%2Fwww5.v1host.com%2FFH-V1%2FSearch.mvc%2FAdvanced%3Fq%3D%7Bquery%7D"
+open "alfred://customsearch/Lookup%20Jira%20by%20ID/jira/utf8/nospace/https%3A%2F%2Falmtools.ldschurch.org%2Ffhjira%2Fbrowse%2F%7Bquery%7D"
