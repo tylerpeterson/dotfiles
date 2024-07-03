99dde9cd148fa41e54e89b774d5b6eb5daeaabc0
Tyler Peterson <petersontb@familysearch.org>
2024-07-03 12:01:04 -0600

    Update import-alfred-searches.sh
    Remove the version one custom search. We don't use that product anymore.

--- import-alfred-searches.sh
+++ import-alfred-searches.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 open "alfred://customsearch/Search%20FHD%20Confluence/confluence/utf8/nospace/https%3A%2F%2Ffhconfluence.churchofjesuschrist.org%2Fdosearchsite.action%3FqueryString%3D%7Bquery%7D"
-open "alfred://customsearch/Search%20FHD%20VersionOne/v1/utf8/nospace/https%3A%2F%2Fwww5.v1host.com%2FFH-V1%2FSearch.mvc%2FAdvanced%3Fq%3D%7Bquery%7D"
 open "alfred://customsearch/Lookup%20Jira%20by%20ID/jira/utf8/nospace/https%3A%2F%2Falmtools.ldschurch.org%2Ffhjira%2Fbrowse%2F%7Bquery%7D"
