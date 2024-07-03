877654ce26fb93893e4d5d3854313f33ecc964e6
Tyler Peterson <petersontb@familysearch.org>
2024-07-03 12:03:45 -0600

    Update import-alfred-searches.sh
    Update URL for searching Jira.

--- import-alfred-searches.sh
+++ import-alfred-searches.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 open "alfred://customsearch/Search%20FHD%20Confluence/confluence/utf8/nospace/https%3A%2F%2Ffhconfluence.churchofjesuschrist.org%2Fdosearchsite.action%3FqueryString%3D%7Bquery%7D"
-open "alfred://customsearch/Lookup%20Jira%20by%20ID/jira/utf8/nospace/https%3A%2F%2Falmtools.ldschurch.org%2Ffhjira%2Fbrowse%2F%7Bquery%7D"
+open "alfred://customsearch/Lookup%20Jira%20by%20ID/jira/utf8/nospace/https://fhjira.churchofjesuschrist.org/browse/%7Bquery%7D"
