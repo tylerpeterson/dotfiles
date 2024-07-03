38f17d85ebccbcfbe29e3d71dcc4b945818ca815
Tyler Peterson <petersontb@familysearch.org>
2024-07-03 12:05:56 -0600

    Update import-alfred-searches.sh
    Add custom search for finding initiatives in Pilot.

--- import-alfred-searches.sh
+++ import-alfred-searches.sh
@@ -2,3 +2,4 @@
 
 open "alfred://customsearch/Search%20FHD%20Confluence/confluence/utf8/nospace/https%3A%2F%2Ffhconfluence.churchofjesuschrist.org%2Fdosearchsite.action%3FqueryString%3D%7Bquery%7D"
 open "alfred://customsearch/Lookup%20Jira%20by%20ID/jira/utf8/nospace/https://fhjira.churchofjesuschrist.org/browse/%7Bquery%7D"
+open "alfred://customsearch/Pilot%20Initiative%20%7Bid%7D/pilot/utf8/nospace/https://beta.familysearch.org/mgmt/pilot/issue/%7Bquery%7D"
